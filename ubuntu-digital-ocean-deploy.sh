41f8cd40670f8e3957108249be1590065e47ab89
Open Crypto Foundation <user@example.com>
2025-04-19 22:15:45 -0400

    Add Digital Ocean deployment scripts

---
+++ ubuntu-digital-ocean-deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Digital Ocean Ubuntu Deployment Script for OpenChain Explorer Node
+# This script will set up the 4th node with the advanced explorer
+# and connect it to the other 3 nodes in the OpenChain network
+
+
+# Colors for output
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+echo -e "Starting OpenChain Node Explorer deployment on Ubuntu..."
+
